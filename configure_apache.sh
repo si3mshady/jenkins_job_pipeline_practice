14d0c3fce6f9402c6d1ad23ed55df9afe2256c11
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-06-18 07:55:50 -0500

    needed break webserver_init into 2 scripts

---
+++ configure_apache.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo mkdir -p /var/www/redpill.com/public_html
+
+sudo chown -R $USER:$USER /var/www/redpill.com/public_html
+
+sudo chmod -R 755 /var/www
+
+sudo a2ensite redpill.com.conf
+sudo a2dissite 000-default.conf
+
+sudo systemctl reload apache2
+sudo systemctl restart apache2
+sudo systemctl status apache2
