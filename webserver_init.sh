b21e19ab4ce13ecc71477972c683a14e27b90527
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-06-18 00:23:48 -0500

    cleaned up webserver_init.sh

--- webserver_init.sh
+++ webserver_init.sh
@@ -2,29 +2,27 @@
 
 sudo apt update
 sudo apt install apache2 -y
-sleep 20
+
 
 sudo mkdir -p /var/www/redpill.com/public_html
-sleep 20
+
 
 sudo mkdir -p /var/www/bluepill.com/public_html
-sleep 20
+
 
 
 sudo chown -R $USER:$USER /var/www/redpill.com/public_html
-sleep 20
+
 
 sudo chown -R $USER:$USER /var/www/bluepill.com/public_html
-sleep 20
+
 
 sudo chmod -R 755 /var/www
-sleep 20
+
 
 sudo touch /var/www/redpill.com/public_html/index.html
-sleep 20
 
 sudo chmod 777  /var/www/redpill.com/public_html/index.html
-sleep 20
 
 cat << FIN > /var/www/redpill.com/public_html/index.html
 <html>
@@ -36,10 +34,6 @@ cat << FIN > /var/www/redpill.com/public_html/index.html
   </body>
 </html>
 FIN
-sleep 5
-
-# cp /var/www/redpill.com/public_html/index.html /var/www/bluepill.com/public_html/index.html
-# sleep 5
 
 
 sudo touch /var/www/bluepill.com/public_html/index.html
@@ -59,10 +53,6 @@ cat << FIN > /var/www/bluepill.com/public_html/index.html
   </body>
 </html>
 FIN 
-sleep 5
-
-# sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/redpill.com.conf
-# sleep 5
 
 sudo chmod 777 /etc/apache2/sites-available/redpill.com.conf
 sleep 5
@@ -79,11 +69,7 @@ cat << FIN > /etc/apache2/sites-available/redpill.com.conf
 </VirtualHost>
 FIN
 
-
-# sudo cp /etc/apache2/sites-available/redpill.com.conf /etc/apache2/sites-available/bluepill.com.conf
-sleep 5
 sudo chmod 777 /etc/apache2/sites-available/bluepill.com.conf
-sleep 5
 
 
 cat << FIN > /etc/apache2/sites-available/bluepill.com.conf
@@ -96,25 +82,10 @@ cat << FIN > /etc/apache2/sites-available/bluepill.com.conf
     CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 FIN
-sleep 5
 
 sudo a2ensite redpill.com.conf
-sleep 5
 sudo a2ensite bluepill.com.conf
-sleep 5
-
 sudo a2dissite 000-default.conf
-sleep 5
-
 sudo systemctl restart apache2
-sleep 5
 sudo systemctl status apache2
-sleep 5
 
-
-# cat << FIN > /etc/hosts 
-# 127.0.0.1   localhost
-# 127.0.1.1   guest-desktop
-# 172.31.62.142 redpill.com
-# 172.31.62.142 bluepill.com
-# FIN
