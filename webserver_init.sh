43f4e60085cbab4e16f51fd3d186d548013f426b
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-06-18 07:54:19 -0500

    needed break webserver_init into 2 scripts

--- webserver_init.sh
+++ webserver_init.sh
@@ -9,27 +9,12 @@ sudo mv ./redpill.com.conf /etc/apache2/sites-available/redpill.com.conf
 localIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 cat << EOF > ./update_hosts.sh
-
 #!/bin/bash
 echo $localIP
-
 cat << FIN > /etc/hosts
 $localIP redpill.com
 FIN
 EOF
-
-
-
-sudo chmod +x ./update_hosts.sh
-sudo ./update_hosts.sh
-
-sudo mkdir -p /var/www/redpill.com/public_html
-
-sudo chown -R $USER:$USER /var/www/redpill.com/public_html
-
-sudo chmod -R 755 /var/www
-
-# sleep 10
 
 cat << FIN > /var/www/redpill.com/public_html/index.html
 <html>
@@ -41,19 +26,6 @@ cat << FIN > /var/www/redpill.com/public_html/index.html
   </body>
 </html>
 FIN 
-
-
-
-sudo a2ensite redpill.com.conf
-
-sudo a2dissite 000-default.conf
-sudo systemctl restart apache2
-sudo systemctl reload apache2
-sudo systemctl status apache2
-
-
-
-
 
 
 #https://www.digitalocean.com/community/tutorials/how-to-set-up-apache-virtual-hosts-on-ubuntu-18-04
