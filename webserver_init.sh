49406bfb2a7058cfc48f7fbff24c5e65f9d053ac
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-06-18 06:44:30 -0500

    sed -i -e 's/bluepill.com/redpill.com/g' webserver_init.sh

--- webserver_init.sh
+++ webserver_init.sh
@@ -7,50 +7,50 @@ sudo apt install apache2 -y
 # sleep 20
 
 
-sudo mkdir -p /var/www/bluepill.com/public_html
+sudo mkdir -p /var/www/redpill.com/public_html
 # sleep 10
 
-sudo chown -R $USER:$USER /var/www/bluepill.com/public_html
+sudo chown -R $USER:$USER /var/www/redpill.com/public_html
 # sleep 10
 
 sudo chmod -R 755 /var/www
 # sleep 10
 
-# sudo touch /var/www/bluepill.com/public_html/index.html
+# sudo touch /var/www/redpill.com/public_html/index.html
 # sleep 10
 
-# sudo chmod 777  /var/www/bluepill.com/public_html/index.html
+# sudo chmod 777  /var/www/redpill.com/public_html/index.html
 sleep 10
 
-cat << FIN > /var/www/bluepill.com/public_html/index.html
+cat << FIN > /var/www/redpill.com/public_html/index.html
 <html>
   <head>
-    <title>Welcome to BluePill.com!</title>
+    <title>Welcome to RedPill.com!</title>
   </head>
   <body>
-    <h1>Success! The bluepill.com virtual host is working!</h1>
+    <h1>Success! The redpill.com virtual host is working!</h1>
   </body>
 </html>
 FIN 
 sleep 10
-sudo touch /etc/apache2/sites-available/bluepill.com.conf
+sudo touch /etc/apache2/sites-available/redpill.com.conf
 sleep 10
-sudo chmod 777 /etc/apache2/sites-available/bluepill.com.conf
+sudo chmod 777 /etc/apache2/sites-available/redpill.com.conf
 sleep 10
 
-cat << FIN > /etc/apache2/sites-available/bluepill.com.conf
+cat << FIN > /etc/apache2/sites-available/redpill.com.conf
 <VirtualHost *:80>
-    ServerAdmin admin@bluepill.com
-    ServerName bluepill.com
-    ServerAlias www.bluepill.com
-    DocumentRoot /var/www/bluepill.com/public_html
+    ServerAdmin admin@redpill.com
+    ServerName redpill.com
+    ServerAlias www.redpill.com
+    DocumentRoot /var/www/redpill.com/public_html
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 FIN
 # sleep 10
 
-sudo a2ensite bluepill.com.conf
+sudo a2ensite redpill.com.conf
 # sleep 5
 
 sudo a2dissite 000-default.conf
@@ -61,12 +61,14 @@ sudo systemctl restart apache2
 
 sudo systemctl status apache2
 
-cat << EOF > update_hosts.sh
-#!/bin/bash
 localIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
+cat << EOF > update_hosts.sh
+
+#!/bin/bash
+echo $localIP
+
 cat << FIN > /etc/hosts
-$localIP bluepill.com
 $localIP redpill.com
 FIN
 EOF
