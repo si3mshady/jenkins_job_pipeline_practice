5696d89f8bb022267bd8e79166844e39a0baa896
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-06-18 00:30:52 -0500

    cleaned up webserver_init.sh

--- webserver_init.sh
+++ webserver_init.sh
@@ -2,26 +2,12 @@
 
 sudo apt update
 sudo apt install apache2 -y
-
-
 sudo mkdir -p /var/www/redpill.com/public_html
-
-
 sudo mkdir -p /var/www/bluepill.com/public_html
-
-
-
 sudo chown -R $USER:$USER /var/www/redpill.com/public_html
-
-
 sudo chown -R $USER:$USER /var/www/bluepill.com/public_html
-
-
 sudo chmod -R 755 /var/www
-
-
 sudo touch /var/www/redpill.com/public_html/index.html
-
 sudo chmod 777  /var/www/redpill.com/public_html/index.html
 
 cat << FIN > /var/www/redpill.com/public_html/index.html
@@ -37,11 +23,7 @@ FIN
 
 
 sudo touch /var/www/bluepill.com/public_html/index.html
-sleep 5
-
 sudo chmod 777  /var/www/bluepill.com/public_html/index.html
-sleep 5
-
 
 cat << FIN > /var/www/bluepill.com/public_html/index.html
 <html>
@@ -55,8 +37,6 @@ cat << FIN > /var/www/bluepill.com/public_html/index.html
 FIN 
 
 sudo chmod 777 /etc/apache2/sites-available/redpill.com.conf
-sleep 5
-
 
 cat << FIN > /etc/apache2/sites-available/redpill.com.conf
 <VirtualHost *:80>
@@ -89,3 +69,4 @@ sudo a2dissite 000-default.conf
 sudo systemctl restart apache2
 sudo systemctl status apache2
 
+#https://www.digitalocean.com/community/tutorials/how-to-set-up-apache-virtual-hosts-on-ubuntu-18-04
