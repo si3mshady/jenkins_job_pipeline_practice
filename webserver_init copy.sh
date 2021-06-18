68ab4334f5a0f291af059c7b17c338f7e4627553
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-06-18 05:32:42 -0500

    added elb

--- webserver_init copy.sh
+++ webserver_init copy.sh
@@ -6,10 +6,14 @@ sleep 20
 sudo apt install apache2 -y
 sleep 20
 
+sudo mkdir -p /var/www/redpill.com/public_html
+sleep 10
 
 sudo mkdir -p /var/www/bluepill.com/public_html
 sleep 10
 
+sudo chown -R $USER:$USER /var/www/redpill.com/public_html
+sleep 10
 
 sudo chown -R $USER:$USER /var/www/bluepill.com/public_html
 sleep 10
@@ -17,12 +21,30 @@ sleep 10
 sudo chmod -R 755 /var/www
 sleep 10
 
+sudo touch /var/www/redpill.com/public_html/index.html
+sleep 10
+
+sudo chmod 777  /var/www/redpill.com/public_html/index.html
+sleep 10
+
+cat << FIN > /var/www/redpill.com/public_html/index.html
+<html>
+  <head>
+    <title>Welcome to RedPill.com!</title>
+  </head>
+  <body>
+    <h1>Success! The redpill.com virtual host is working!</h1>
+  </body>
+</html>
+FIN
+sleep 10
+
 sudo touch /var/www/bluepill.com/public_html/index.html
 sleep 10
 
 sudo chmod 777  /var/www/bluepill.com/public_html/index.html
-sleep 10
 
+sleep 10
 cat << FIN > /var/www/bluepill.com/public_html/index.html
 <html>
   <head>
@@ -33,6 +55,21 @@ cat << FIN > /var/www/bluepill.com/public_html/index.html
   </body>
 </html>
 FIN 
+
+sudo chmod 777 /etc/apache2/sites-available/redpill.com.conf
+
+cat << FIN > /etc/apache2/sites-available/redpill.com.conf
+<VirtualHost *:80>
+    ServerAdmin admin@redpill.com
+    ServerName redpill.com
+    ServerAlias www.redpill.com
+    DocumentRoot /var/www/redpill.com/public_html
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+FIN
+
+sleep 10
 
 sudo chmod 777 /etc/apache2/sites-available/bluepill.com.conf
 sleep 10
@@ -49,6 +86,9 @@ cat << FIN > /etc/apache2/sites-available/bluepill.com.conf
 FIN
 sleep 10
 
+sudo a2ensite redpill.com.conf
+sleep 5
+
 sudo a2ensite bluepill.com.conf
 sleep 5
 
@@ -60,5 +100,7 @@ sleep 5
 
 sudo systemctl status apache2
 
+echo redpill.com
+echo bluepill.com 
 
 #https://www.digitalocean.com/community/tutorials/how-to-set-up-apache-virtual-hosts-on-ubuntu-18-04
